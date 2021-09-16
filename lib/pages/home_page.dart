6b2a56b9c4ba9371bd55cd07f7e298dee25ca9a5
Elvis Barrionuevo <idcareg.g@gmail.com>
2021-09-16 18:59:38 -0500

    Header App

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,14 +7,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
-  initState(){
+  initState() {
     super.initState();
     getData();
   }
 
-  getData() async{
+  getData() async {
     String path = "http://192.168.100.5:8000/api/task-list/";
     Uri _uri = Uri.parse(path);
     http.Response response = await http.get(_uri);
@@ -24,14 +23,46 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-
-          ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Bienvenido",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      Text(
+                        "My Tasks",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 40.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  CircleAvatar(
+                    radius: 26,
+                    backgroundImage: NetworkImage("https://images.pexels.com/photos/3310693/pexels-photo-3310693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
